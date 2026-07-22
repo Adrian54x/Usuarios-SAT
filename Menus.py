d2b42ec10fe84a5b0cc3f647037580178d42f5c8
ricogm-source <ricardoggmenchu@gmail.com>
2026-07-22 17:42:54 -0600

    Agregado de los menus dinamicos

---
+++ Menus.py
@@ -0,0 +1,20 @@
+def menu_principal():
+    print("1. Recepcion")
+    print("2. Empleados")
+
+def recepcion():
+    print("1. Registro Tributario Unificado (RTU) y NIT")
+    print("2. Registro Fiscal de Vehículos (RFV)")
+    print("3. Convenios de pago, multas e impugnaciones (Recaudacion)")
+    print("4. Orientación, agencia virtual y FEL")
+    print("5. Recepción de expedientes y ventanilla preferencial")
+
+def empleados():
+    print("1. Atender cliente")
+    print("2. Ver cola")
+    print("3. Ver historial")
+    print("4. Eliminar persona")
+    print("5. Modificar datos")
+    print("6. Ver cantidad de personas en espera")
+    print("7. Ver cantidad de personas en historial")
+    print("8. Buscar persona en historial")
