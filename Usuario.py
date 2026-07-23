ceaa2306ada3070e4601402aa9d25605b4319aff
adria <adria@REX54X>
2026-07-22 18:08:04 -0600

    Iniciacion de validaciones y de clase validaciones

--- Usuario.py
+++ Usuario.py
@@ -1,5 +1,6 @@
+
 class Usuarios:
-    def __init__(self, dpi, nombres, apellidos, pais, fechaDeNacimiento, genero):
+    def __init__(self, dpi: int, nombres: str, apellidos: str, pais: str, fechaDeNacimiento: str, genero: str):
         self.DPI = dpi
         self.Nombres = nombres
         self.Apellidos = apellidos
@@ -30,3 +31,13 @@ class Usuarios:
     @property
     def Genero(self):
         return self.Genero
+
+    @Dpi.setter
+    def Dpi(self, dpi: int):
+        if len(str(dpi)) == 13 and dpi in [1,2,3,4,5,6,7,8,9,0]:
+            self.DPI = dpi
+        else:
+            raise ValueError("DPI no valido!")
+
+    @Nombres.setter
+    #def Nombres(self, nombres: str):
