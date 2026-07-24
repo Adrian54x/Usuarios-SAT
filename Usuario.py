55075dd665608a6b7784ad0aea9455061ea987db
adria <adria@REX54X>
2026-07-23 22:01:35 -0600

    Clase usuario terminada

--- Usuario.py
+++ Usuario.py
@@ -1,43 +1,65 @@
+from datetime import datetime
+import Listas
 
 class Usuarios:
-    def __init__(self, dpi: int, nombres: str, apellidos: str, pais: str, fechaDeNacimiento: str, genero: str):
-        self.DPI = dpi
+    def __init__(self, dpi: int, nombres: str, apellidos: str, fechaDeNacimiento: datetime, genero: str):
+        self.Dpi = dpi
         self.Nombres = nombres
         self.Apellidos = apellidos
-        self.Pais = pais
         self.FechaDeNacimiento = fechaDeNacimiento
         self.Genero = genero
 
     @property
     def Dpi(self):
-        return self.DPI
+        return self.__DPI
 
     @property
     def Nombres(self):
-        return self.Nombres
+        return self.__Nombres
 
     @property
     def Apellidos(self):
-        return self.Apellidos
-
-    @property
-    def Pais(self):
-        return self.Pais
+        return self.__Apellidos
 
     @property
     def FechaDeNacimiento(self):
-        return self.FechaDeNacimiento
+        return self.__FechaDeNacimiento
 
     @property
     def Genero(self):
-        return self.Genero
+        return self.__Genero
 
     @Dpi.setter
     def Dpi(self, dpi: int):
-        if len(str(dpi)) == 13 and dpi in [1,2,3,4,5,6,7,8,9,0]:
-            self.DPI = dpi
+        if len(str(dpi)) == 13:
+            self.__DPI = dpi
         else:
-            raise ValueError("DPI no valido!")
+            print("DPI no valido!")
 
     @Nombres.setter
-    #def Nombres(self, nombres: str):
+    def Nombres(self, nombres: str):
+        if nombres[0] != " " and len(nombres) >= 7:
+            self.__Nombres = nombres
+        else:
+            print("Nombres no validos!")
+
+    @Apellidos.setter
+    def Apellidos(self, apellidos: str):
+        if apellidos[0] != " " and len(apellidos) >= 7 :
+            self.__Apellidos = apellidos
+        else:
+            print("Apellidos no validos!")
+
+    @FechaDeNacimiento.setter
+    def FechaDeNacimiento(self, fechaDeNacimiento: datetime):
+        if (datetime.today().year - fechaDeNacimiento.year > 18) and (datetime.today().year - fechaDeNacimiento.year <= 100):
+            self.__FechaDeNacimiento = fechaDeNacimiento
+        else:
+            print("Fecha de nacimiento no valido!")
+
+    @Genero.setter
+    def Genero(self, genero: str):
+        if genero in Listas.LGenero().Extraer():
+            self.__Genero = genero
+        else:
+            print("Genero no valido!")
