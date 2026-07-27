e62ad2034fe6f6346e92d3b1821a7867e88c44fa
adria <adria@REX54X>
2026-07-27 17:37:34 -0600

    modificacion de codigo

--- programa.py
+++ programa.py
@@ -1,15 +1,25 @@
-import Usuario
 import Validaciones
 
-usuario = Usuario.Usuarios()
 validacion = Validaciones.Validacion()
 
 while True:
     try:
-        opcion = validacion.ValInt("Elija una opcion", "Opcion invalida!")
+        opcion = validacion.ValInt("Elija una opcion:", "Opcion invalida!")
         match opcion:
             case 1:
-                print("=== Usuario ===")
+                subOpcion = validacion.ValInt("Elija una opcion:", "Opcion invalida!")
+                if(subOpcion not in [1,2,3,4,5]):
+                    print("Opcion invalida")
+                else:
+                    print("=== Usuario ===")
+                    dpi = validacion.Dpi()
+                    nombres = validacion.Nombre()
+                    apellidos = validacion.Apellido()
+                    fechaDeNacimiento = validacion.FechaNacimiento()
+                    genero = validacion.Genero()
+                    match subOpcion:
+                        case _:
+                            print("Opcion no existente!")
             case _:
                 print("Opcion no existente")
     except:
