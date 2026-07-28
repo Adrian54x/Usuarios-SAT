8c1903f9e245c19003a0e6206eda4cd6803e59be
adria <adria@REX54X>
2026-07-27 21:14:10 -0600

    Se agrgo mas listas

--- Listas.py
+++ Listas.py
@@ -1,3 +1,6 @@
+from turtledemo.penrose import start
+
+
 class Listas:
     def __init__(self, lista : list):
         if(lista == []):
@@ -8,7 +11,30 @@ class Listas:
     def Extraer(self):
         return self.__Lista
 
+    def Mostar(self):
+        for c,x in enumerate(self.__Lista, start=1):
+            print(f"{c}. {x}")
+
+
 class LGenero(Listas):
     def __init__(self):
-        Genero = ["Hombre", "Mujer"]
-        super().__init__(Genero)
+        __Genero = ["Hombre", "Mujer"]
+        super().__init__(__Genero)
+
+class LCategoria(Listas):
+    def __init__(self):
+        __Categorias = ["Tramite", "Actualizacion", "Registro"]
+        super().__init__(__Categorias)
+
+class LCategoriaOP1(Listas):
+    def __init__(self):
+        __Categorias1 = ["Nit", "RTU"]
+        super().__init__(__Categorias1)
+
+class LCategoriaOP2(Listas):
+    class LCategoriaOP1(Listas):
+        def __init__(self):
+            __Categorias1 = ["Vheiculo", "Placas"]
+            super().__init__(__Categorias1)
+
+
