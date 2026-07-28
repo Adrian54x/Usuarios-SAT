c1e9d18bbd9fb9ac73fb9d5a9c12d9f77b3d467c
adria <adria@REX54X>
2026-07-27 22:43:56 -0600

    se implementaron algunos metodos

--- Colas.py
+++ Colas.py
@@ -3,3 +3,24 @@ from collections import deque
 class Cola:
     def __init__(self):
         self.__Cola = deque()
+
+    def Push(self, agregar):
+        self.__Cola.append(agregar)
+
+    def Mostrar(self):
+        for n, c in enumerate(self.__Cola, start=1):
+            c.Mostrar()
+
+    def EliminarPaciente(self, dpi):
+        for p in self.__Cola:
+            if p.Dpi == dpi:
+                self.__Cola.remove(p)
+                return True
+        return False
+
+    def PopCola(self):
+        return self.__cola.popleft()
+
+    def CantidadPila(self):
+        return len(self.__pila)
+
--- Pilas.py
+++ Pilas.py
@@ -1,3 +1,10 @@
 class Pilas:
     def __init__(self, pila : list):
-        self.__Lista = pila
+        self.__Pila = pila
+
+    def Push(self, agregar):
+        self.__Pila.append(agregar)
+
+    def Mostrar(self):
+        for n, c in enumerate(self.__Pila, start=1):
+            c.Mostrar()
